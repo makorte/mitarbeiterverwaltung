d964844509f42c15f4760b00524d97353040aeee
Max Korte <privat@max-korte.de>
2021-06-13 11:55:02 +0200

    initial commit

---
+++ src/main/java/com/maxkorte/mitarbeiterverwaltung/MitarbeiterverwaltungApplication.java
@@ -0,0 +1,13 @@
+package com.maxkorte.mitarbeiterverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitarbeiterverwaltungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MitarbeiterverwaltungApplication.class, args);
+    }
+
+}
